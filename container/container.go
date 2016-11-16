4d0a54845896ef0377b09f32fe94762304bf8989
Brandon Powell <bpowell29a@gmail.com>
2016-11-15 21:02:19 -0500

    Added comments to package container

--- container/container.go
+++ container/container.go
@@ -10,6 +10,7 @@ import (
 	"time"
 )
 
+// Contains all information needed to run a container.
 type Container struct {
 	Name        string
 	ServiceName string `json:"service-name"`
@@ -22,6 +23,7 @@ type Container struct {
 	VEth        string
 }
 
+// SetName sets the name of the container based on start time and the command being run.
 func (c *Container) SetName() {
 	value := fmt.Sprintf("%s%s", c.StartTime, c.Command)
 	sha := sha1.New()
@@ -29,6 +31,7 @@ func (c *Container) SetName() {
 	c.Name = hex.EncodeToString(sha.Sum(nil))[:8]
 }
 
+// Close shutdowns the container.
 func (c *Container) Close() {
 	if err := c.Exec("/bin/umount /app"); err != nil {
 		fmt.Println("Cannot unmount /app: ", err)
@@ -38,6 +41,7 @@ func (c *Container) Close() {
 	p.Kill()
 }
 
+// Exec executes command inside of the container.
 func (c *Container) Exec(cmd string) error {
 	command := strings.Split(fmt.Sprintf("nsenter --target %d --pid --net --mount %s", c.Pid, cmd), " ")
 	if err := exec.Command(command[0], command[1:]...).Run(); err != nil {
